da63a762c8ef3c0fc2ecc6a4573593a75bb91955
pravakar <pravupravakarraopravu@outlook.com>
2023-01-21 17:10:39 +0530

    initial commit

---
+++ CricketBEnd/src/main/java/com/pravakar/learning/SpringLearningApplication.java
@@ -0,0 +1,13 @@
+package com.pravakar.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
